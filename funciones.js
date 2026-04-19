e04f9f70e840fa01ac870676bd455e956ed3b4a2
danielorrala555-spce <danielorrala555@gmail.com>
2026-04-19 18:26:29 -0500

    correction of typing errors

--- funciones.js
+++ funciones.js
@@ -1,1 +1,18 @@
-//AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos,egresos){
+    let valorDisponible=0
+    valorDisponible=ingresos-egresos;
+    if(valorDisponible <0){
+        return 0;
+
+    }
+    return valorDisponible
+
+}
+function calcularCapacidadPago(valorDisponoible){
+    let resultado;
+    resultado = valorDisponible /2;
+
+    return resultado;
+
+
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,23 @@
-//AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular(){
+    let ingresosFloat=0;
+    let egresosFloat=0;
+    let cmpIngresosFloat;
+    let cmpEgresosFloat;
+    let total;
+    let saldoDisponible;
+ 
+    cmpIngresosFloat=document.getElementById("txtIngresos");
+    cmpEgresosFloat=document.getElementById("txtEgresos");
+    ingresosFloat=parseFloat(cmpIngresosFloat.value);
+    egresosFloat=parseFloat(cmpEgresosFloat.value);
+ 
+    saldoDisponible=calcularDisponible(ingresosFloat,egresosFloat);
+    total=document.getElementById("spnDisponible");
+    total.innerText=saldoDisponible;
+ 
+    let capacidadDePago=calcularCapacidadPago(saldoDisponible);
+    let mostrarCapacidadPago=document.getElementById("spnCapacidadPago");
+    mostrarCapacidadPago.innerText=capacidadDePago;
+ 
+ 
+}
