a7c41162551feed798a282473a2f51bb746d4b1a
danielorrala555-spce <danielorrala555@gmail.com>
2026-09-19 17:46:52 -0500

    add the calcular function

--- funciones.js
+++ funciones.js
@@ -1,1 +1,6 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos, egresos){
+    let resultado = ingresos - egresos;
+    return resultado
+
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,31 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function recupearTexto(idComponente){
+
+    let componente = document.getElementById(idComponente);
+    let valor = componente.value;
+
+    return valor;   
+}
+
+function recuperarFloat(idComponente){
+    let valorTexto = recupearTexto(idComponente);
+    let valorFloat = parseFloat(valorTexto);
+    return valorFloat;
+}
+function mostarEnEspam(idComponente, valor){
+    let componente = document.getElementById(idComponente);
+     componente.textContent = valor;
+
+}
+
+function calcular(){
+    let ingresos = recuperarFloat("txtIngresos")
+    let egresos = recuperarFloat("txtEgresos")
+    let disponible = calcularDisponible(ingresos, egresos);
+    if(disponible < 0){
+        disponible = 0
+    }
+    mostarEnEspam("spnCapacidadPago", disponible)
+    
+
+}
